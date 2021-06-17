d7c0f4a50df140259b4b9ac3306f0432505c9af8
Jake <jbourne2021@fau.edu>
2021-06-17 10:55:26 -0400

    fixed user struct

--- users.go
+++ users.go
@@ -1,20 +1,147 @@
 package goadafruit
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 type User struct {
-	ID        int    `json:"id"`
-	Name      string `json:"name"`
-	Color     string `json:"color"`
-	Username  string `json:"username"`
-	TimeZone  string `json:"time_zone"`
-	CreatedAt string `json:"created_at"`
-	UpdatedAt string `json:"updated_at"`
+	User struct {
+		ID                 int         `json:"id"`
+		Name               string      `json:"name"`
+		Color              string      `json:"color"`
+		Username           string      `json:"username"`
+		Role               string      `json:"role"`
+		DefaultGroupID     int         `json:"default_group_id"`
+		DefaultDashboardID interface{} `json:"default_dashboard_id"`
+		TimeZone           string      `json:"time_zone"`
+		CreatedAt          time.Time   `json:"created_at"`
+		UpdatedAt          time.Time   `json:"updated_at"`
+		Subscription       struct {
+			Status string `json:"status"`
+			Limits struct {
+				Feeds      int `json:"feeds"`
+				Dashboards int `json:"dashboards"`
+				Groups     int `json:"groups"`
+				DataTTL    int `json:"data_ttl"`
+				DataRate   int `json:"data_rate"`
+			} `json:"limits"`
+			Plan struct {
+				Name       string `json:"name"`
+				Price      int    `json:"price"`
+				Interval   string `json:"interval"`
+				StripeID   string `json:"stripe_id"`
+				BaseLimits struct {
+					Feeds      int `json:"feeds"`
+					Dashboards int `json:"dashboards"`
+					Groups     int `json:"groups"`
+					DataTTL    int `json:"data_ttl"`
+					DataRate   int `json:"data_rate"`
+				} `json:"base_limits"`
+				Free bool `json:"free"`
+				Paid bool `json:"paid"`
+			} `json:"plan"`
+		} `json:"subscription"`
+		BetaFlags interface{} `json:"beta_flags"`
+	} `json:"user"`
+	Profile struct {
+		Name              string        `json:"name"`
+		Username          string        `json:"username"`
+		TimeZone          string        `json:"time_zone"`
+		CreatedAt         time.Time     `json:"created_at"`
+		ConnectedAccounts []interface{} `json:"connected_accounts"`
+		Subscription      struct {
+			ID   interface{} `json:"id"`
+			Plan struct {
+				Name       string `json:"name"`
+				Price      int    `json:"price"`
+				Interval   string `json:"interval"`
+				StripeID   string `json:"stripe_id"`
+				BaseLimits struct {
+					Feeds      int `json:"feeds"`
+					Dashboards int `json:"dashboards"`
+					Groups     int `json:"groups"`
+					DataTTL    int `json:"data_ttl"`
+					DataRate   int `json:"data_rate"`
+				} `json:"base_limits"`
+				Free bool `json:"free"`
+				Paid bool `json:"paid"`
+			} `json:"plan"`
+			CreatedAt interface{} `json:"created_at"`
+			Status    string      `json:"status"`
+			Limits    struct {
+				Feeds      int `json:"feeds"`
+				Dashboards int `json:"dashboards"`
+				Groups     int `json:"groups"`
+				DataTTL    int `json:"data_ttl"`
+				DataRate   int `json:"data_rate"`
+			} `json:"limits"`
+			Price    int           `json:"price"`
+			Upgrades []interface{} `json:"upgrades"`
+		} `json:"subscription"`
+		HasBillingHistory bool        `json:"has_billing_history"`
+		HasPaymentSource  bool        `json:"has_payment_source"`
+		AccountBalance    int         `json:"account_balance"`
+		BetaDiscount      interface{} `json:"beta_discount"`
+		ActionItems       struct {
+			PendingShares []interface{} `json:"pending_shares"`
+		} `json:"action_items"`
+		PromotionDiscounts []interface{} `json:"promotion_discounts"`
+		Coupons            []interface{} `json:"coupons"`
+	} `json:"profile"`
+	Sidebar struct {
+		FeedCount      int `json:"feed_count"`
+		GroupCount     int `json:"group_count"`
+		DashboardCount int `json:"dashboard_count"`
+		ActiveDataRate int `json:"active_data_rate"`
+	} `json:"sidebar"`
+	Navigation struct {
+		Feeds struct {
+			Records []struct {
+				ID        int       `json:"id"`
+				Name      string    `json:"name"`
+				Key       string    `json:"key"`
+				UpdatedAt time.Time `json:"updated_at"`
+				LastValue string    `json:"last_value"`
+			} `json:"records"`
+			Count int `json:"count"`
+		} `json:"feeds"`
+		Dashboards struct {
+			Records []struct {
+				ID        int       `json:"id"`
+				Name      string    `json:"name"`
+				UpdatedAt time.Time `json:"updated_at"`
+				Key       string    `json:"key"`
+			} `json:"records"`
+			Count int `json:"count"`
+		} `json:"dashboards"`
+		Devices struct {
+			Records []interface{} `json:"records"`
+		} `json:"devices"`
+		Triggers struct {
+			Records []interface{} `json:"records"`
+		} `json:"triggers"`
+	} `json:"navigation"`
+	Throttle struct {
+		DataRateLimit              int         `json:"data_rate_limit"`
+		ActiveDataRate             int         `json:"active_data_rate"`
+		AuthenticationRate         int         `json:"authentication_rate"`
+		SubscribeAuthorizationRate int         `json:"subscribe_authorization_rate"`
+		PublishAuthorizationRate   int         `json:"publish_authorization_rate"`
+		HourlyBanRate              int         `json:"hourly_ban_rate"`
+		MqttBanErrorMessage        interface{} `json:"mqtt_ban_error_message"`
+	} `json:"throttle"`
+	SystemMessages []interface{} `json:"system_messages"`
 }
 
 type UserDetails struct {
-	DataRateLimit  int `json:"data_rate_limit"`
-	ActiveDataRate int `json:"active_data_rate"`
+	DataRateLimit              int         `json:"data_rate_limit"`
+	ActiveDataRate             int         `json:"active_data_rate"`
+	AuthenticationRate         int         `json:"authentication_rate"`
+	SubscribeAuthorizationRate int         `json:"subscribe_authorization_rate"`
+	PublishAuthorizationRate   int         `json:"publish_authorization_rate"`
+	HourlyBanRate              int         `json:"hourly_ban_rate"`
+	MqttBanErrorMessage        interface{} `json:"mqtt_ban_error_message"`
 }
 
 type UserService struct {
@@ -22,7 +149,7 @@ type UserService struct {
 }
 
 func (s *UserService) GetUserInformation() (*User, *Response, error) {
-	path := fmt.Sprint("/api/v2/user")
+	path := "/api/v2/user"
 
 	req, rerr := s.client.NewRequest("GET", path, nil)
 	if rerr != nil {
--- users_test.go
+++ users_test.go
@@ -17,12 +17,165 @@ func TestUserInformation(t *testing.T) {
 			testMethod(t, r, "GET")
 			fmt.Fprint(w, `
 			{
-				"id": 1,
-				"name": "Jake",
-				"color": "blue",
-				"username": "JakeFau",
-				"time_zone": "EST"
-			}`)
+				"user": {
+				  "id": 509745,
+				  "name": "Jacob Bourne",
+				  "color": "#1433e3",
+				  "username": "JakeFau",
+				  "role": "user",
+				  "default_group_id": 435375,
+				  "default_dashboard_id": null,
+				  "time_zone": "America/New_York",
+				  "created_at": "2021-05-14T01:59:27Z",
+				  "updated_at": "2021-06-17T18:07:14Z",
+				  "subscription": {
+					"status": "active",
+					"limits": {
+					  "feeds": 10,
+					  "dashboards": 5,
+					  "groups": 5,
+					  "data_ttl": 2592000,
+					  "data_rate": 30
+					},
+					"plan": {
+					  "name": "Free",
+					  "price": 0,
+					  "interval": "month",
+					  "stripe_id": "io-free",
+					  "base_limits": {
+						"feeds": 10,
+						"dashboards": 5,
+						"groups": 5,
+						"data_ttl": 2592000,
+						"data_rate": 30
+					  },
+					  "free": true,
+					  "paid": false
+					}
+				  },
+				  "beta_flags": null
+				},
+				"profile": {
+				  "name": "Jacob Bourne",
+				  "username": "JakeFau",
+				  "time_zone": "America/New_York",
+				  "created_at": "2021-05-14T01:59:27Z",
+				  "connected_accounts": [],
+				  "subscription": {
+					"id": null,
+					"plan": {
+					  "name": "Free",
+					  "price": 0,
+					  "interval": "month",
+					  "stripe_id": "io-free",
+					  "base_limits": {
+						"feeds": 10,
+						"dashboards": 5,
+						"groups": 5,
+						"data_ttl": 2592000,
+						"data_rate": 30
+					  },
+					  "free": true,
+					  "paid": false
+					},
+					"created_at": null,
+					"status": "free",
+					"limits": {
+					  "feeds": 10,
+					  "dashboards": 5,
+					  "groups": 5,
+					  "data_ttl": 2592000,
+					  "data_rate": 30
+					},
+					"price": 0,
+					"upgrades": []
+				  },
+				  "has_billing_history": false,
+				  "has_payment_source": false,
+				  "account_balance": 0,
+				  "beta_discount": null,
+				  "action_items": {
+					"pending_shares": []
+				  },
+				  "promotion_discounts": [],
+				  "coupons": []
+				},
+				"sidebar": {
+				  "feed_count": 5,
+				  "group_count": 3,
+				  "dashboard_count": 1,
+				  "active_data_rate": 0
+				},
+				"navigation": {
+				  "feeds": {
+					"records": [
+					  {
+						"id": 1638924,
+						"name": "Weather / Humidity",
+						"key": "weather.humidity",
+						"updated_at": "2021-06-16T16:04:09Z",
+						"last_value": "5.10e+01"
+					  },
+					  {
+						"id": 1640907,
+						"name": "Outdoor / Temperature",
+						"key": "outdoor.temperature",
+						"updated_at": "2021-06-16T16:04:09Z",
+						"last_value": "28.9"
+					  },
+					  {
+						"id": 1640909,
+						"name": "Outdoor / Humidity",
+						"key": "outdoor.humidity",
+						"updated_at": "2021-06-16T16:04:09Z",
+						"last_value": "75.0"
+					  },
+					  {
+						"id": 1638911,
+						"name": "Weather / Temperature",
+						"key": "weather.temperature",
+						"updated_at": "2021-06-16T16:04:09Z",
+						"last_value": "7.56e+01"
+					  },
+					  {
+						"id": 1638925,
+						"name": "Weather / Pressure",
+						"key": "weather.pressure",
+						"updated_at": "2021-06-16T16:04:09Z",
+						"last_value": "3.00e+01"
+					  }
+					],
+					"count": 5
+				  },
+				  "dashboards": {
+					"records": [
+					  {
+						"id": 445654,
+						"name": "Jake's Apt",
+						"updated_at": "2021-06-17T09:33:07Z",
+						"key": "jakes-apt"
+					  }
+					],
+					"count": 1
+				  },
+				  "devices": {
+					"records": []
+				  },
+				  "triggers": {
+					"records": []
+				  }
+				},
+				"throttle": {
+				  "data_rate_limit": 30,
+				  "active_data_rate": 0,
+				  "authentication_rate": 0,
+				  "subscribe_authorization_rate": 0,
+				  "publish_authorization_rate": 0,
+				  "hourly_ban_rate": 0,
+				  "mqtt_ban_error_message": null
+				},
+				"system_messages": []
+			  }`)
 		})
 
 	assert := assert.New(t)
@@ -33,7 +186,7 @@ func TestUserInformation(t *testing.T) {
 	assert.NotNil(user)
 	assert.NotNil(response)
 
-	assert.Equal("Jake", user.Name)
+	assert.Equal("Jacob Bourne", user.User.Name)
 
 }
 
